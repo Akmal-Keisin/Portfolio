a0a1d6c7ca21a9d971ba541fa9e7bdc683f44e54
Akmal Keisin <akmalkeisin@gmail.com>
2026-04-27 19:23:47 +0700

    ADD - design

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'http://localhost:4321',
 	integrations: [mdx(), sitemap()],
+
 	fonts: [
 		{
 			provider: fontProviders.local(),
@@ -32,4 +35,7 @@ export default defineConfig({
 			},
 		},
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
